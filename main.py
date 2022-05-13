e5262da895cb9e6ba8dbbefc97bf8db25cf47907
Kevin Bähr <kevinbaehr03@gmail.com>
2022-05-13 23:55:34 +0200

    Added Acceleration and Deceleration

--- main.py
+++ main.py
@@ -15,14 +15,11 @@ SCREEN_HEIGHT = 442
 SCREEN_TITLE = "AI-Racing"
 
 ACCELERATION = 0.1
-MAX_MOVEMENT_SPEED = 10
-MOVEMENT_SPEED = 2
+DECELERATION = 0.05
+MAX_SPEED = 3
 ANGLE_SPEED = 2
-START_TIME = 0
-END_TIME = 0
-DELTA_TIME = 0
+FRICTION = 0.005
 
-winner = None
 
 class Player1(arcade.Sprite):
 
@@ -30,11 +27,24 @@ class Player1(arcade.Sprite):
 
         super().__init__(image, scale)
 
-        self.max_speed = 10
         self.speed = 0
-        self.acceleration = 0.1
 
     def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        if self.left < 0:
+            self.left = 0
+            self.change_x = 0  # Zero x speed
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+            self.change_x = 0
+
+        if self.bottom < 0:
+            self.bottom = 0
+            self.change_y = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+            self.change_y = 0
         angle_rad = math.radians(self.angle)
 
         self.angle += self.change_angle
@@ -43,12 +53,10 @@ class Player1(arcade.Sprite):
         self.center_y += self.speed * math.cos(angle_rad)
 
 
-
 class MyGame(arcade.Window):
 
-
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT,SCREEN_TITLE)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         self.player1_list = None
         self.wall_list = None
@@ -58,6 +66,11 @@ class MyGame(arcade.Window):
         self.p1_health = None
 
         self.background = None
+
+        self.W = False
+        self.A = False
+        self.S = False
+        self.D = False
 
         self.set_mouse_visible(False)
 
@@ -70,49 +83,82 @@ class MyGame(arcade.Window):
 
         self.p1_health = P1_MAX_HEALTH
 
-        self.player1_sprite = Player1("Mclaren Daniel Riccardo.png",SPRITE_SCALING_PLAYERS)
+        self.player1_sprite = Player1("Mclaren Daniel Riccardo.png", SPRITE_SCALING_PLAYERS)
         self.player1_sprite.center_x = SCREEN_WIDTH/2
         self.player1_sprite.center_y = 50
         self.player1_list.append(self.player1_sprite)
 
         arcade.set_background_color(arcade.color.BLACK)
 
-        #self.physics_engine1 = arcade.PhysicsEngineSimple(self.player1_sprite, self.wall_list)
-
     def on_draw(self):
         arcade.start_render()
 
-        arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH,SCREEN_HEIGHT, self.background)
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        arcade.draw_text(f"Speed: {self.player1_sprite.speed:6.3f}", 10, 50, arcade.color.BLACK)
 
         self.player1_list.draw()
 
     def on_update(self, delta_time):
+        if self.player1_sprite.speed > FRICTION:
+            self.player1_sprite.speed -= FRICTION
+        elif self.player1_sprite.speed < -FRICTION:
+            self.player1_sprite.speed += FRICTION
+        else:
+            self.player1_sprite.speed = 0
 
+        if self.player1_sprite.speed > FRICTION:
+            self.player1_sprite.speed -= FRICTION
+        elif self.player1_sprite.speed < -FRICTION:
+            self.player1_sprite.speed += FRICTION
+        else:
+            self.player1_sprite.speed = 0
+
+        if self.W and not self.S:
+            self.player1_sprite.speed += ACCELERATION
+        elif self.S and not self.W:
+            self.player1_sprite.speed += -DECELERATION
+        if self.A and not self.D:
+            self.player1_sprite.change_angle = ANGLE_SPEED
+        elif self.D and not self.A:
+            self.player1_sprite.change_angle = -ANGLE_SPEED
+
+        if self.player1_sprite.speed > MAX_SPEED:
+            self.player1_sprite.speed = MAX_SPEED
+        elif self.player1_sprite.speed < -MAX_SPEED:
+            self.player1_sprite.speed = -MAX_SPEED
+        if self.player1_sprite.speed > MAX_SPEED:
+            self.player1_sprite.speed = MAX_SPEED
+        elif self.player1_sprite.speed < -MAX_SPEED:
+            self.player1_sprite.speed = -MAX_SPEED
         self.player1_list.update()
 
     def on_key_press(self, key, modifiers):
 
         if key == arcade.key.W:
-            if MOVEMENT_SPEED < MAX_MOVEMENT_SPEED:
-                self.player1_sprite.speed = MOVEMENT_SPEED
-                movement_speed = MOVEMENT_SPEED + ACCELERATION
-            else:
-                self.player1_sprite.speed = MOVEMENT_SPEED
-
+            self.W = True
         elif key == arcade.key.S:
-            self.player1_sprite.speed = -MOVEMENT_SPEED
+            self.S = True
         elif key == arcade.key.A:
-            self.player1_sprite.change_angle = ANGLE_SPEED
+            self.A = True
         elif key == arcade.key.D:
-            self.player1_sprite.change_angle = -ANGLE_SPEED
+            self.D = True
+
+
+
 
     def on_key_release(self, key, modifiers):
-        if key == arcade.key.W or key == arcade.key.S:
-            self.player1_sprite.speed = 0
-        elif key == arcade.key.A or key == arcade.key.D:
+
+        if key == arcade.key.W:
+            self.W = False
+        elif key == arcade.key.S:
+            self.S = False
+        elif key == arcade.key.A:
+            self.A = False
+        elif key == arcade.key.D:
+            self.D = False
+
+        if key == arcade.key.A or key == arcade.key.D:
             self.player1_sprite.change_angle = 0
-
-
 
 
 
